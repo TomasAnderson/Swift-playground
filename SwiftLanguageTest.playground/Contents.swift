95dc286d128a830fc28452cfa49308d87cccb95e
Zhou You <yollyhideout@gmail.com>
2016-11-18 11:47:02 +0800

    init repo

---
+++ SwiftLanguageTest.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+/*
+ 
+ Constant and Variables
+ 
+*/
+
+//constant and variables
+
+var variable = 10
+
+let constant = 10.2
+
+//type annotation
+let welcomeMessage: String = "Hello world!"
+
+var a, b, c: Double
+
+//naming constants and variables
+let 你好 = "世界你好！"
+let 🐷 = "妳才是！"
+
+
+//print
+print("The chinese version of hello-world message is \(你好)")
+
